97560a9632ec9640e17c4098a1af1549d60b55c6
valere chihisa <vchihisa@gmail.com>
2023-01-02 14:38:18 +0200

    implement add to cart button

--- .history/pages/_document_20230102143055.tsx
+++ .history/pages/_document_20230102143055.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='h-screen bg-gray-100 flex items-center justify-center '>
+      <body className='h-screen bg-gray-100 flex items-center justify-between '>
         <Main />
         <NextScript />
       </body>
