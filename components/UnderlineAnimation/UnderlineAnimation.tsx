d7f15806ff84c00f30da2a6ff35d94017909f332
valere chihisa <vchihisa@gmail.com>
2023-01-24 21:52:13 +0200

    minor update

--- components/UnderlineAnimation/UnderlineAnimation.tsx
+++ components/UnderlineAnimation/UnderlineAnimation.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import styles from '../../styles/Home.module.css'
+import React from "react";
+import styles from "../../styles/Home.module.css";
 
 function UnderlineAnimation() {
   return (
     <div className="flex h-[60vh]   justify-center items-center">
       <a href="" className={styles.underlineAnimation}>
-        Underline Animation
+        I usign this animation just for fun
       </a>
     </div>
   );
 }
 
-export  {UnderlineAnimation}
+export { UnderlineAnimation };
